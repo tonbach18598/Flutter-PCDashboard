0367a02f84ea80e2e8195a5b8fe2f1d7add96372
Bach <tonbach18598@gmail.com>
2020-02-09 10:47:37 +0700

    fix contact, study UI

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -11,138 +11,134 @@ class ContactScreen extends StatefulWidget {
 class _ContactScreenState extends State<ContactScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
             child: InkWell(
-              borderRadius: BorderRadius.only(
-                  topLeft:
-                      Radius.circular(MediaQuery.of(context).size.height / 8.8),
-                  bottomRight: Radius.circular(
-                      MediaQuery.of(context).size.height / 8.8)),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.deepOrange,
-                        Colors.deepOrangeAccent,
-                        Colors.orange,
-                        Colors.orangeAccent,
-                      ],
-                      begin: FractionalOffset.topCenter,
-                      end: FractionalOffset.bottomCenter),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(
-                          MediaQuery.of(context).size.height / 8),
-                      bottomRight: Radius.circular(
-                          MediaQuery.of(context).size.height / 8)),
+                borderRadius: BorderRadius.only(
+                    topLeft:
+                        Radius.circular(MediaQuery.of(context).size.height / 8.8),
+                    bottomRight: Radius.circular(
+                        MediaQuery.of(context).size.height / 8.8)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          Colors.deepOrange,
+                          Colors.deepOrangeAccent,
+                          Colors.orange,
+                          Colors.orangeAccent,
+                        ],
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(
+                            MediaQuery.of(context).size.height / 8),
+                        bottomRight: Radius.circular(
+                            MediaQuery.of(context).size.height / 8)),
+                  ),
+                  child: Center(
+                      child: Text(
+                    Values.CHAT.toUpperCase(),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  )),
                 ),
-                child: Center(
-                    child: Text(
-                  Values.CHAT.toUpperCase(),
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                )),
+                onTap: () async {
+                  Navigator.of(context).pushNamed(Routes.chatRoute,
+                      arguments: (await Preferences.loadSelf()).userId);
+                },
               ),
-              onTap: () async {
-                Navigator.of(context).pushNamed(Routes.chatRoute,
-                    arguments: (await Preferences.loadSelf()).userId);
-              },
             ),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+          SizedBox(height: 5,),
+          Expanded(
+            flex: 1,
             child: InkWell(
-              borderRadius: BorderRadius.only(
-                  topRight:
-                      Radius.circular(MediaQuery.of(context).size.height / 8.8),
-                  bottomLeft: Radius.circular(
-                      MediaQuery.of(context).size.height / 8.8)),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.green,
-                        Colors.lightGreen,
-                        Colors.lightGreenAccent,
-                        Colors.greenAccent,
-                      ],
-                      begin: FractionalOffset.topCenter,
-                      end: FractionalOffset.bottomCenter),
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(
-                          MediaQuery.of(context).size.height / 8),
-                      bottomLeft: Radius.circular(
-                          MediaQuery.of(context).size.height / 8)),
+                borderRadius: BorderRadius.only(
+                    topRight:
+                        Radius.circular(MediaQuery.of(context).size.height / 8.8),
+                    bottomLeft: Radius.circular(
+                        MediaQuery.of(context).size.height / 8.8)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          Colors.green,
+                          Colors.lightGreen,
+                          Colors.lightGreenAccent,
+                          Colors.greenAccent,
+                        ],
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter),
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(
+                            MediaQuery.of(context).size.height / 8),
+                        bottomLeft: Radius.circular(
+                            MediaQuery.of(context).size.height / 8)),
+                  ),
+                  child: Center(
+                      child: Text(
+                    Values.STUDENT_LIST.toUpperCase(),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  )),
                 ),
-                child: Center(
-                    child: Text(
-                  Values.STUDENT_LIST.toUpperCase(),
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                )),
+                onTap: () {
+                  Navigator.of(context)
+                      .pushNamed(Routes.userRoute, arguments: 'SV');
+                },
               ),
-              onTap: () {
-                Navigator.of(context)
-                    .pushNamed(Routes.userRoute, arguments: 'SV');
-              },
             ),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+          SizedBox(height: 5,),
+          Expanded(
+            flex: 1,
             child: InkWell(
-              borderRadius: BorderRadius.only(
-                  topLeft:
-                      Radius.circular(MediaQuery.of(context).size.height / 8.8),
-                  bottomRight: Radius.circular(
-                      MediaQuery.of(context).size.height / 8.8)),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.blueAccent,
-                        Colors.blue,
-                        Colors.lightBlue,
-                        Colors.lightBlueAccent,
-                      ],
-                      begin: FractionalOffset.topCenter,
-                      end: FractionalOffset.bottomCenter),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(
-                          MediaQuery.of(context).size.height / 8),
-                      bottomRight: Radius.circular(
-                          MediaQuery.of(context).size.height / 8)),
+                borderRadius: BorderRadius.only(
+                    topLeft:
+                        Radius.circular(MediaQuery.of(context).size.height / 8.8),
+                    bottomRight: Radius.circular(
+                        MediaQuery.of(context).size.height / 8.8)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.blue,
+                          Colors.lightBlue,
+                          Colors.lightBlueAccent,
+                        ],
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(
+                            MediaQuery.of(context).size.height / 8),
+                        bottomRight: Radius.circular(
+                            MediaQuery.of(context).size.height / 8)),
+                  ),
+                  child: Center(
+                      child: Text(
+                    Values.TEACHER_LIST.toUpperCase(),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  )),
                 ),
-                child: Center(
-                    child: Text(
-                  Values.TEACHER_LIST.toUpperCase(),
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                )),
+                onTap: () {
+                  Navigator.of(context)
+                      .pushNamed(Routes.userRoute, arguments: 'GV');
+                },
               ),
-              onTap: () {
-                Navigator.of(context)
-                    .pushNamed(Routes.userRoute, arguments: 'GV');
-              },
             ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/screens/study_screen.dart
+++ lib/screens/study_screen.dart
@@ -10,93 +10,91 @@ class StudyScreen extends StatefulWidget {
 class _StudyScreenState extends State<StudyScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
             child: InkWell(
-              borderRadius: BorderRadius.only(
-                  topLeft:
-                      Radius.circular(MediaQuery.of(context).size.height / 4.4),
-                  topRight: Radius.circular(
-                      MediaQuery.of(context).size.height / 4.4)),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(
-                          MediaQuery.of(context).size.height / 4),
-                      topRight: Radius.circular(
-                          MediaQuery.of(context).size.height / 4)),
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.deepOrange,
-                        Colors.deepOrangeAccent,
-                        Colors.orange,
-                        Colors.orangeAccent,
-                      ],
-                      begin: FractionalOffset.topCenter,
-                      end: FractionalOffset.bottomCenter),
+                borderRadius: BorderRadius.only(
+                    topLeft:
+                        Radius.circular(MediaQuery.of(context).size.height / 4.4),
+                    topRight: Radius.circular(
+                        MediaQuery.of(context).size.height / 4.4)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(
+                            MediaQuery.of(context).size.height / 4),
+                        topRight: Radius.circular(
+                            MediaQuery.of(context).size.height / 4)),
+                    gradient: LinearGradient(
+                        colors: [
+                          Colors.deepOrange,
+                          Colors.deepOrangeAccent,
+                          Colors.orange,
+                          Colors.orangeAccent,
+                        ],
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter),
+                  ),
+                  child: Center(
+                      child: Text(
+                    Values.SCHEDULE.toUpperCase(),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  )),
                 ),
-                child: Center(
-                    child: Text(
-                  Values.SCHEDULE.toUpperCase(),
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                )),
+                onTap: () {
+                  Navigator.of(context).pushNamed(Routes.scheduleRoute);
+                },
               ),
-              onTap: () {
-                Navigator.of(context).pushNamed(Routes.scheduleRoute);
-              },
             ),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+          SizedBox(height: 5,),
+          Expanded(
+            flex: 1,
             child: InkWell(
-              borderRadius: BorderRadius.only(
-                  bottomLeft:
-                      Radius.circular(MediaQuery.of(context).size.height / 4.4),
-                  bottomRight: Radius.circular(
-                      MediaQuery.of(context).size.height / 4.4)),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(
-                          MediaQuery.of(context).size.height / 4),
-                      bottomRight: Radius.circular(
-                          MediaQuery.of(context).size.height / 4)),
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.blueAccent,
-                        Colors.blue,
-                        Colors.lightBlue,
-                        Colors.lightBlueAccent,
-                      ],
-                      begin: FractionalOffset.topCenter,
-                      end: FractionalOffset.bottomCenter),
+                borderRadius: BorderRadius.only(
+                    bottomLeft:
+                        Radius.circular(MediaQuery.of(context).size.height / 4.4),
+                    bottomRight: Radius.circular(
+                        MediaQuery.of(context).size.height / 4.4)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(
+                            MediaQuery.of(context).size.height / 4),
+                        bottomRight: Radius.circular(
+                            MediaQuery.of(context).size.height / 4)),
+                    gradient: LinearGradient(
+                        colors: [
+                          Colors.blueAccent,
+                          Colors.blue,
+                          Colors.lightBlue,
+                          Colors.lightBlueAccent,
+                        ],
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter),
+                  ),
+                  child: Center(
+                      child: Text(
+                    Values.EXAM.toUpperCase(),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  )),
                 ),
-                child: Center(
-                    child: Text(
-                  Values.EXAM.toUpperCase(),
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                )),
+                onTap: () {
+                  Navigator.of(context).pushNamed(Routes.examRoute);
+                },
               ),
-              onTap: () {
-                Navigator.of(context).pushNamed(Routes.examRoute);
-              },
             ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
